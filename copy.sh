1ff72fa770b4ee89476af4d8b0d6e6357c5185a0
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-28 14:44:52 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -4,7 +4,7 @@ END_INDEX=30
 
 for i in $(seq $START_INDEX $END_INDEX); do
     echo "Copying to Profile$i"
-    cp -rf ~/.mozilla/firefox/Profile2/* ~/.mozilla/firefox/Profile$i
+    cp -rf Profile2/* ~/.mozilla/firefox/Profile$i
 done
 
 echo "Profiles copied ."
