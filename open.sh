257e26f507e86d26e40e56517c78c09a0b4160ec
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-28 13:15:57 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -3,7 +3,7 @@ START_INDEX=3
 END_INDEX=30
 
 for i in $(seq $START_INDEX $END_INDEX); do
-   firefox -p Profile
+   firefox -p Profile$i &>/dev/null &
 done
 
 echo "Profiles copied from $SOURCE_PROFILE to $DESTINATION_PREFIX{${START_INDEX}..${END_INDEX}}."
